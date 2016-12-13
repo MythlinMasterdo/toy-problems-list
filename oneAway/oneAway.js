b1c02adfd361bbadddea6b92dc499d78828f21f6
Miles Smith <miles71397@gmail.com>
2016-12-13 13:52:01 -0800

    work on oneAway

--- oneAway/oneAway.js
+++ oneAway/oneAway.js
@@ -3,5 +3,12 @@
 //"pale" "plae" false
 
 var oneAway = function(str1, str2) {
-  
+  var splitStr1 = str1.split("");
+  var splitStr2 = str2.split("");
+  if(splitStr1.length - splitStr2.length > 1 || splitStr2.length - splitStr1.length > 1) {
+    return false;
+  }
+  for(var i = 0; i < splitStr1.length; i++) {
+    
+  }
 }
