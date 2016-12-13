b1c01db83cee03579cae2121ea92dd3abcc387a3
Miles Smith <miles71397@gmail.com>
2016-12-13 14:07:15 -0800

    work on zeroMatrix

--- zeroMatrix/zeroMatrix.js
+++ zeroMatrix/zeroMatrix.js
@@ -1,4 +1,13 @@
 // make a function such that if an element in an M*N matrix is 0 that row and column are set to 0
 var zeroMatrix = function(matrix) {
-  
+  for(var i = 0; i < matrix.length; i++) {
+    for(var j = 0; j < matrix[i].length; j++) {
+      if(matrix[i][j] === 0) {
+        for(var k = 0; k < matrix[i][j].length; k++) {
+          matrix[i][j] = 0;
+        }
+        
+      }
+    }
+  }
 }
