63b4ae65f8913f8ee3f120ee8e9099431fcdf86b
Miles Smith <miles71397@gmail.com>
2016-12-13 14:11:15 -0800

    complete zeroMatrix

--- zeroMatrix/zeroMatrix.js
+++ zeroMatrix/zeroMatrix.js
@@ -3,11 +3,15 @@ var zeroMatrix = function(matrix) {
   for(var i = 0; i < matrix.length; i++) {
     for(var j = 0; j < matrix[i].length; j++) {
       if(matrix[i][j] === 0) {
-        for(var k = 0; k < matrix[i][j].length; k++) {
-          matrix[i][j] = 0;
+        for(var k = 0; k < matrix[i].length; k++) {
+          matrix[i][k] = 0;
         }
-        
+        for(var l = 0; l < matrix.length; l++) {
+          matrix[l][i] = 0;
+        }
       }
     }
   }
+  return matrix;
 }
+console.log(zeroMatrix([[1,1,1,1],[1,1,1,1],[0,1,1,1]]));
