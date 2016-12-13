274048818c5a5aee638db962c1da3f38a492a4b0
Miles Smith <miles71397@gmail.com>
2016-12-13 13:58:05 -0800

    finish oneAway

--- oneAway/oneAway.js
+++ oneAway/oneAway.js
@@ -5,10 +5,26 @@
 var oneAway = function(str1, str2) {
   var splitStr1 = str1.split("");
   var splitStr2 = str2.split("");
+  var shortest;
+  if(splitStr1.length < splitStr2.length) {
+    shortest = splitStr1;
+  } else {
+    shortest = splitStr2;
+  }
+  var dif = 0;
   if(splitStr1.length - splitStr2.length > 1 || splitStr2.length - splitStr1.length > 1) {
     return false;
   }
-  for(var i = 0; i < splitStr1.length; i++) {
-    
+  for(var i = 0; i < shortest.length; i++) {
+    if(splitStr1[i] !== splitStr2[i]) {
+      dif++;
+    }
   }
+  if(dif > 1) {
+    return false;
+  }
+  return true;
 }
+console.log(oneAway("hello", "helo"));
+console.log(oneAway("hello", "heool"));
+console.log(oneAway("Miles", "miles"));
