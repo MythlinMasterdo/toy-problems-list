6aa68f91cfa5f660b0d4d97dbf92d65edac414fd
Miles Smith <miles71397@gmail.com>
2016-12-15 13:38:23 -0800

    complete smallestK

--- smallestK/smallestK.js
+++ smallestK/smallestK.js
@@ -1,4 +1,12 @@
 //create a function that returns the smallest k numbers in an array
-var smallestK = function(k) {
-  
+var smallestK = function(k, arr) {
+  var sortedArr = arr.sort(function(a,b) {
+    return a-b;
+  })
+  var smallestKResult = [];
+  for(var i = 0; i < k; i++) {
+    smallestKResult.push(sortedArr[i]);
+  }
+  return smallestKResult;
 }
+console.log(smallestK(4, [1,6,9,7,42,67,87,4]));
