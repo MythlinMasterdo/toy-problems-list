d412c379be24913dbc79694eaa63a73e1cb6894b
Miles Smith <miles71397@gmail.com>
2016-12-15 13:44:48 -0800

    start work on calculator

---
+++ calculator/calculator.js
@@ -0,0 +1,6 @@
+//create a method that takes in a arithmetic equation of positive integers (no parentheses)
+//compute the result input: 2*3+5/6*3+15 output: 23.5
+
+var calculator = function(equation) {
+  
+}
